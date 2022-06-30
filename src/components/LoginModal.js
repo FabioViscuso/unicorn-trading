87fadc4a9d7cf92a85aa308715aa116728c095db
Marco Moncada <marco.monkey93@gmail.com>
2022-06-30 11:03:59 +0200

    feat: favicon
    added favicon

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -28,9 +28,9 @@ export function LoginModal({ closeModal, user, modalSetLogin }) {
 
     return (
         <>
-            <div className="py-10 bg-gray-900 text-4xl transition duration-150 ease-in-out z-10 fixed top-0 right-0 bottom-0 left-0" id="modal">
-                <div role="alert" className="container mx-auto w-11/12 md:w-8/10 max-w-lg">
-                    <div className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+            <div className="py-10 bg-[#33300333] text-4xl transition duration-150 ease-in-out z-10 fixed top-0 right-0 bottom-0 left-0" id="modal">
+                <div role="alert" className="container mx-auto  w-11/12 md:w-8/10 max-w-lg">
+                    <div className="relative py-8 px-5 md:px-10 bg-[#f7d6e0] shadow-md rounded-3xl border border-gray-400">
 
                         <h1 className="text-gray-600 font-lg font-bold tracking-normal leading-tight mb-4">Login</h1>
                         <label htmlFor="loginUsername" className="text-gray-800 text-md font-bold leading-tight tracking-normal">Username</label>
